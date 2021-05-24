362b3c4633ea07f9b865bd80f553f648f6a2705e
satrajitghosh183 <83156880+satrajitghosh183@users.noreply.github.com>
2021-05-25 00:43:17 +0530

    Add files via upload
    This is to change the date into a number between 1 and 365

---
+++ DateConversion.py
@@ -0,0 +1,20 @@
+import calendar
+import time
+days_added=[31,59,90,120,151,181,212,243,273,304,334]
+import datetime
+
+date=input("Enter date yyyy-mm-dd")
+
+datee = datetime.datetime.strptime(a, "%Y-%m-%d")
+
+
+month=datee.month
+day= datee.day
+year=datee.year
+cultve_date=day
+
+cultve_date=day+days_added[x-2]
+print(z)
+print(year)
+
+
